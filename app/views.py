a018d408551ed0df7b4217d1331f8636772dc3a4
Alex Martins da Silva <14134062624@muzambinho.edu.br>
2023-11-28 22:29:40 -0300

    feat: add navbar

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from . models import *
 def login(request):
     
     return render(request, 'auth/login.html')
+
+
+def categorias(request):
+     return render(request, 'categorias.html')
--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,6 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login, name='login'),
-    path('login/',login , name='login')
+    path('login/',login , name='login'),
+    path('categorias/', categorias, name='categorias'),
 ]
